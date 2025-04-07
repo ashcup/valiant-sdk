940de5688d55a737be02c0b4fea7d37fa3d82382
ashlynn-juniper <160202125+voidvoxel@users.noreply.github.com>
2025-04-07 12:42:53 -0500

    Add functions to raylua.h

--- include/raylua.h
+++ include/raylua.h
@@ -59,6 +59,7 @@ typedef lua_Writer LuaWriter;
 #define LuaPop(L, n)                            lua_pop(L, n)
 
 void LuaSetGlobalFunction(LuaState* L, const char* name, LuaCFunction f);
+void LuaSetGlobalInteger(LuaState* L, const char* name, int i);
 
 void LuaSetGlobalFunction(LuaState* L, const char* name, LuaCFunction f)
 {
@@ -68,6 +69,60 @@ void LuaSetGlobalFunction(LuaState* L, const char* name, LuaCFunction f)
     // Get the value on top of the stack
     // and set as a global, in this case is the function
     lua_setglobal(L, name);
+}
+
+void LuaSetGlobalInteger(LuaState* L, const char* name, int i)
+{
+    // Push the integer
+    lua_pushinteger(L, i);
+
+    // Get the value on top of the stack
+    // and set as a global, in this case is the function
+    lua_setglobal(L, name);
+}
+
+void LuaSetGlobalString(LuaState* L, const char* name, const char* s)
+{
+    // Push the string
+    lua_pushstring(L, s);
+
+    // Get the value on top of the stack
+    // and set as a global, in this case is the function
+    lua_setglobal(L, name);
+}
+
+int LuaLoadModule(LuaState* L, const char* modulePath)
+{
+    int errorCode = LUA_OK;
+
+	if (!FileExists(modulePath))
+	{
+		TraceLog(LOG_ERROR, "`init.lua` not found.");
+	}
+
+    const char* sourceCode = LoadFileText(modulePath);
+
+	if (errorCode = LuaLoadString(L, sourceCode) != LUA_OK)
+	{
+        TraceLog(LOG_ERROR, "Unsupported string encoding used for source code.");
+	}
+
+	if (
+        (errorCode == LUA_OK) &&
+        (errorCode = LuaPCall(L, 0, 0, 0) != LUA_OK)
+    )
+	{
+		TraceLog(LOG_ERROR, "Invalid source code.");
+	}
+
+	if (errorCode == LUA_OK)
+    {
+        LuaPop(L, LuaGetTop(L));
+    }
+
+    UnloadFileText(sourceCode);
+
+    return errorCode;
 }
 
 #endif // RAYLUA_H
