d7f35cf86596e3b8aef939f47e50123d667f6d9a
ashlynn-juniper <160202125+voidvoxel@users.noreply.github.com>
2025-04-07 12:42:24 -0500

    Update init.lua

--- examples/hello-world/init.lua
+++ examples/hello-world/init.lua
@@ -1,1 +1,9 @@
-Log("Hello, world!")
+-- This event handler is called when the program starts.
+on("start", function ()
+    Log("Hello, world!")
+end)
+
+-- This event handler is called when the program stops.
+on("stop", function ()
+    Log("Goodbye, world!")
+end)
