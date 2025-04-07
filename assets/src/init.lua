77724b88cdf31c2c3acffc1c3855b278d8bb7d6a
ashlynn-juniper <160202125+voidvoxel@users.noreply.github.com>
2025-04-07 12:42:15 -0500

    Update init.lua

--- assets/src/init.lua
+++ assets/src/init.lua
@@ -1,1 +1,9 @@
-Log("Hello, Valiant!")
+-- Require the `application` and `console` modules.
+local application   = require "valiant.application"
+local console       = require "valiant.console"
+
+-- Log a message.
+console.Log("Hello, Valiant!")
+
+-- Exit the application.
+application.Exit(0)
