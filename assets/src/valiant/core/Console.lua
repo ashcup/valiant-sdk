d984b3d9d3d7deb54441f310807a5f4b1b5341eb
ashlynn-juniper <160202125+voidvoxel@users.noreply.github.com>
2025-04-09 07:35:34 -0500

    Create Console.lua

---
+++ assets/src/valiant/core/Console.lua
@@ -0,0 +1,15 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+local class = require "valiant.core.class"
+
+local Console = class {
+    logLevel = LOG_INFO
+}
+
+function Console:log(message)
+    vstdlib_console_log(message)
+end
+
+return Console
