4b56f96d0524d59d553e8a52a3bc817eef0104f7
ashlynn-juniper <160202125+voidvoxel@users.noreply.github.com>
2025-04-09 07:34:22 -0500

    Add function `tableutils.copy`

--- assets/src/valiant/core/tableutils.lua
+++ assets/src/valiant/core/tableutils.lua
@@ -2,11 +2,17 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-local function merge(t1, t2)
+local tableutils = {}
+
+function tableutils.copy(t)
+    local u = { }
+    for k, v in pairs(t) do u[k] = v end
+    return setmetatable(u, debug.getmetatable(t))
+  end
+
+function tableutils.merge(t1, t2)
     for k,v in pairs(t2) do t1[k] = v end
     return t1
 end
 
-return {
-    merge = merge
-}
+return tableutils
